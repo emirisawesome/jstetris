faa58ca0c82b88fd5fcfda383980fda0931d7131
Emir <emirisaev2000@gmail.com>
2020-12-13 17:20:31 +0600

    i need hugs

--- assets/index.js
+++ assets/index.js
@@ -1,31 +1,14 @@
 import Game from './src/game.js';
 import View from './src/view.js';
+import Controller from './src/controller.js';
 
 const element = document.querySelector('#master');
 
 const game = new Game();
 const view = new View(element,  480, 640, 20, 10);
+const controller = new Controller(game, view);
 
 window.game = game;
 window.view = view;
+window.controller = controller;
 
-document.addEventListener('keydown', event => {
-    switch (event.keyCode){
-        case 37:
-            game.moveTetrominoLeft();
-            view.render(game.state());
-            break;
-        case 38:
-            game.rotateTetromino();
-            view.render(game.state());
-            break;
-        case 39:
-            game.moveTetrominoRight();
-            view.render(game.state());
-            break;
-        case 40:
-            game.moveTetrominoDown();
-            view.render(game.state());
-            break;
-    }
-})
--- assets/src/controller.js
+++ assets/src/controller.js
@@ -1,5 +1,33 @@
 export default class Controller {
     constructor(game, view) {
+        this.game = game;
+        this.view = view;
 
+        document.addEventListener('keydown', this.handleKeyDown.bind(this));
+
+        this.view.renderStartScreen();
+    }
+
+    handleKeyDown(event) {
+        switch (event.keyCode){
+            case 13:
+                this.view.renderMainScreen(this.game.state());
+            case 37:
+                this.game.moveTetrominoLeft();
+                this.view.renderMainScreen(game.state());
+                break;
+            case 38:
+                game.rotateTetromino();
+                view.renderMainScreen(game.state());
+                break;
+            case 39:
+                this.game.moveTetrominoRight();
+                this.view.renderMainScreen(game.state());
+                break;
+            case 40:
+                this.game.moveTetrominoDown();
+                this.view.renderMainScreen(game.state());
+                break;
+        }
     }
 }
